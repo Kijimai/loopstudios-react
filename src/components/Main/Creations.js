4693dd840abddbb818b746788e115167d807d41b
Jibbyworks <JDBucog@yahoo.com>
2021-11-11 13:18:40 -0800

    complete creations section responsiveness

--- src/components/Main/Creations.js
+++ src/components/Main/Creations.js
@@ -6,26 +6,24 @@ const Creations = () => {
 
   return (
     <section className="creations">
-      <div className="creations-inner-container">
+      <div className="creations-header-container">
         <h2>Our Creations</h2>
-        <button className="seeAll-btn">See All</button>
-        <div className="creations-content-container">
-          {creationsData.map((creation, index) => {
-            return (
-              <div key={index} className="creation-box">
-                <img
-                  className="creation-img"
-                  src={
-                    viewWidth > 768 ? creation.imgDesktop : creation.imgMobile
-                  }
-                  alt={creation.title}
-                />
-                <h3>{creation.title}</h3>
-              </div>
-            )
-          })}
-        </div>
-        <button className="seeAll-btn">See All</button>
+        <button className="seeAll-btn btn1">See All</button>
+      </div>
+      <div className="creations-content-container">
+        {creationsData.map((creation, index) => {
+          return (
+            <div key={index} className="creation-box">
+              <img
+                className="creation-img"
+                src={viewWidth > 768 ? creation.imgDesktop : creation.imgMobile}
+                alt={creation.title}
+              />
+              <h3>{creation.title}</h3>
+            </div>
+          )
+        })}
+        <button className="seeAll-btn btn2">See All</button>
       </div>
     </section>
   )
